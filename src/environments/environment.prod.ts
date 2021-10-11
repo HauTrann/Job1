bcc6c9c00c2069b7a2592e5bf852aef7df741480
Hautv <Hauvantran97@gmail.com>
2021-10-11 21:32:16 +0700

    Update v1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ADMIN: 'admin',
-  // API_URL: 'http://localhost:3000/'
-  API_URL: 'https://sleepy-fjord-77052.herokuapp.com/'
+  API_URL: 'http://localhost:3000/'
+  // API_URL: 'https://sleepy-fjord-77052.herokuapp.com/'
 };
